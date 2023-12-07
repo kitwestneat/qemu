c443997d396804444b151aadd9e5c234f08faa95
Kit Westneat <kit.westneat@gmail.com>
2023-12-07 23:15:55 +0000

    Covert spaces to tabs in niova dev

--- block/niova.c
+++ block/niova.c
@@ -30,8 +30,8 @@ typedef struct NiovaDevState NiovaDevState;
 
 struct NiovaDevState {
 	niova_block_client_t *client;
-    struct niova_block_client_xopts xopts;
-    int64_t vdev_size;
+	struct niova_block_client_xopts xopts;
+	int64_t vdev_size;
 
 	// uuid_t uuid; should use target_uuid on the opts
 	struct {
@@ -60,59 +60,59 @@ struct NiovaDevState {
 #define NIOVA_OPT_QUEUE_DEPTH "queue-depth"
 
 static QemuOptsList runtime_opts = {
-    .name = "niova",
-    .head = QTAILQ_HEAD_INITIALIZER(runtime_opts.head),
-    .desc = {
-        {
-            .name = NIOVA_OPT_UUID,
-            .type = QEMU_OPT_STRING,
-            .help = "UUID",
-        },
-        {
-            .name = NIOVA_OPT_VDEV,
-            .type = QEMU_OPT_STRING,
-            .help = "vdev UUID",
-        },
-        {
-            .name = NIOVA_OPT_QUEUE_DEPTH,
-            .type = QEMU_OPT_NUMBER,
-            .help = "Queue Depth",
-        },
-        { /* end of list */ }
-    },
+	.name = "niova",
+	.head = QTAILQ_HEAD_INITIALIZER(runtime_opts.head),
+	.desc = {
+		{
+			.name = NIOVA_OPT_UUID,
+			.type = QEMU_OPT_STRING,
+			.help = "UUID",
+		},
+		{
+			.name = NIOVA_OPT_VDEV,
+			.type = QEMU_OPT_STRING,
+			.help = "vdev UUID",
+		},
+		{
+			.name = NIOVA_OPT_QUEUE_DEPTH,
+			.type = QEMU_OPT_NUMBER,
+			.help = "Queue Depth",
+		},
+		{ /* end of list */ }
+	},
 };
 
 /* Parse a connection in the format of niova://[uuid][#queue_depth]. Example:
  *
- *     niova://uuid#32
+ *	 niova://uuid#32
  */
 static void niovadev_parse_filename(const char *filename, QDict *options,
-                                   Error **errp)
+								   Error **errp)
 {
-    int pref = strlen("niova://");
-    if (strlen(filename) <= pref || strncmp(filename, "niova://", pref)) {
-            return;
-    }
+	int pref = strlen("niova://");
+	if (strlen(filename) <= pref || strncmp(filename, "niova://", pref)) {
+			return;
+	}
 
-    const char *pref_end = filename + pref;
-    const char *slash = strchr(pref_end, '/');
+	const char *pref_end = filename + pref;
+	const char *slash = strchr(pref_end, '/');
 	const char *uuid_end = slash;
-    if (!uuid_end) {
-        qdict_put_str(options, NIOVA_OPT_UUID, pref_end);
-        return;
-    }
+	if (!uuid_end) {
+		qdict_put_str(options, NIOVA_OPT_UUID, pref_end);
+		return;
+	}
 
 	char *uuid = g_strndup(pref_end, uuid_end - pref_end);
 	qdict_put_str(options, NIOVA_OPT_UUID, uuid);
 	g_free(uuid);
 
-    const char *vdev_start = uuid_end + 1;
-    const char *pound = strchr(vdev_start, '#');
+	const char *vdev_start = uuid_end + 1;
+	const char *pound = strchr(vdev_start, '#');
 	const char *vdev_end = pound;
-    if (!vdev_end) {
-        qdict_put_str(options, NIOVA_OPT_VDEV, vdev_start);
-        return;
-    }
+	if (!vdev_end) {
+		qdict_put_str(options, NIOVA_OPT_VDEV, vdev_start);
+		return;
+	}
 
 	char *vdev = g_strndup(uuid_end, vdev_end - vdev_start);
 	qdict_put_str(options, NIOVA_OPT_VDEV, vdev);
@@ -136,11 +136,11 @@ static void niovadev_parse_filename(const char *filename, QDict *options,
 
 static void niovadev_close(BlockDriverState *bs)
 {
-    NiovaDevState *s = bs->opaque;
+	NiovaDevState *s = bs->opaque;
 	if (s->client)
 		NiovaBlockClientDestroy(s->client);
 
-    return;
+	return;
 }
 
 static int niova_client_setup(NiovaDevState *s) {
@@ -162,12 +162,12 @@ static int niova_client_setup(NiovaDevState *s) {
 }
 
 static int niovadev_file_open(BlockDriverState *bs, QDict *options, int flags,
-                             Error **errp)
+							 Error **errp)
 {
-    QemuOpts *opts = qemu_opts_create(&runtime_opts, NULL, 0, &error_abort);
-    qemu_opts_absorb_qdict(opts, options, &error_abort);
+	QemuOpts *opts = qemu_opts_create(&runtime_opts, NULL, 0, &error_abort);
+	qemu_opts_absorb_qdict(opts, options, &error_abort);
 
-    NiovaDevState *s = bs->opaque;
+	NiovaDevState *s = bs->opaque;
 
 	// XXX do these strdups need to be freed?
 	const char *uuid = qemu_opt_get(opts, NIOVA_OPT_UUID);
@@ -189,37 +189,37 @@ static int niovadev_file_open(BlockDriverState *bs, QDict *options, int flags,
 		return rc;
 	}
 
-    bs->supported_write_flags = BDRV_REQ_FUA;
+	bs->supported_write_flags = BDRV_REQ_FUA;
 
-    return 0;
+	return 0;
 }
 
 static int64_t niovadev_getlength(BlockDriverState *bs)
 {
-    NiovaDevState *s = bs->opaque;
-    return s->vdev_size;
+	NiovaDevState *s = bs->opaque;
+	return s->vdev_size;
 }
 
 static int niovadev_probe_blocksizes(BlockDriverState *bs, BlockSizes *bsz)
 {
-    bsz->phys = NIOVADEV_BLOCK_SIZE;
-    bsz->log = NIOVADEV_BLOCK_SIZE;
-    return 0;
+	bsz->phys = NIOVADEV_BLOCK_SIZE;
+	bsz->log = NIOVADEV_BLOCK_SIZE;
+	return 0;
 }
 
 struct niovadev_cb_data {
-    Coroutine *co;
+	Coroutine *co;
 	ssize_t ret;
-    AioContext *ctx;
+	AioContext *ctx;
 };
 
 // qemu context
 static void niovadev_rw_cb_bh(void *arg)
 {
 	struct niovadev_cb_data *data = arg;
-    if (!data->co) {
+	if (!data->co) {
 		aio_bh_schedule_oneshot(data->ctx, niovadev_rw_cb_bh, data);
-    } else {
+	} else {
 		qemu_coroutine_enter(data->co);
 	}
 }
@@ -228,14 +228,14 @@ static void niovadev_rw_cb_bh(void *arg)
 static void niovadev_rw_cb(void *arg, ssize_t rc) {
 	struct niovadev_cb_data *data = arg;
 	data->ret = rc;
-    aio_bh_schedule_oneshot(data->ctx, niovadev_rw_cb_bh, data);
+	aio_bh_schedule_oneshot(data->ctx, niovadev_rw_cb_bh, data);
 }
 
 static coroutine_fn int niovadev_co_rw(bool is_write, BlockDriverState *bs,
-                                      int64_t start_512_blk, int nblk,
-                                      QEMUIOVector *qiov)
+									  int64_t start_512_blk, int nblk,
+									  QEMUIOVector *qiov)
 {
-    NiovaDevState *s = bs->opaque;
+	NiovaDevState *s = bs->opaque;
 	if (start_512_blk % 8) {
 		fprintf(stderr, "startblk not aligned %ld\n", start_512_blk);
 		return -EINVAL;
@@ -253,7 +253,7 @@ static coroutine_fn int niovadev_co_rw(bool is_write, BlockDriverState *bs,
 		s->stats.qd_max = s->stats.qd_cur;
 
 	struct niovadev_cb_data cb_data = {
-        .ctx = bdrv_get_aio_context(bs),
+		.ctx = bdrv_get_aio_context(bs),
 		.ret = -EINPROGRESS,
 	};
 
@@ -279,11 +279,11 @@ static coroutine_fn int niovadev_co_rw(bool is_write, BlockDriverState *bs,
 	}
 
 	cb_data.co = qemu_coroutine_self();
-    AioContext *co_ctx = qemu_coroutine_get_aio_context(cb_data.co);
+	AioContext *co_ctx = qemu_coroutine_get_aio_context(cb_data.co);
 	fprintf(stderr, "yielding, equal ctx? %s\n", co_ctx == cb_data.ctx ? "yes" : "no");
 	do {
-        qemu_coroutine_yield();
-    } while (cb_data.ret == -EINPROGRESS);
+		qemu_coroutine_yield();
+	} while (cb_data.ret == -EINPROGRESS);
 	fprintf(stderr, "done yielding, ret=%zd\n", cb_data.ret);
 	s->stats.qd_cur--;
 
@@ -324,7 +324,7 @@ static coroutine_fn int niovadev_co_rw(bool is_write, BlockDriverState *bs,
 	if (qiov->niov > s->stats.iov_cnt_max)
 		s->stats.iov_cnt_max = qiov->niov;
 
-    return 0;
+	return 0;
 }
 
 static coroutine_fn int niovadev_co_readv(BlockDriverState *bs,
@@ -347,60 +347,60 @@ static coroutine_fn int niovadev_co_flush(BlockDriverState *bs)
 {
 	// XXX: todo
 
-    return 0;
+	return 0;
 }
 
 static int coroutine_fn niovadev_co_truncate(BlockDriverState *bs, int64_t offset,
-                                            bool exact, PreallocMode prealloc,
-                                            BdrvRequestFlags flags, Error **errp)
+											bool exact, PreallocMode prealloc,
+											BdrvRequestFlags flags, Error **errp)
 {
 	// XXX: todo
-    return 0;
+	return 0;
 }
 
 static int coroutine_fn niovadev_co_pdiscard(BlockDriverState *bs,
-                                             int64_t offset,
-                                             int64_t bytes)
+											 int64_t offset,
+											 int64_t bytes)
 {
 	fprintf(stderr, "discard: offset %ld bytes %ld\n", offset, bytes);
 	return 0;
 }
 
 static int niovadev_reopen_prepare(BDRVReopenState *reopen_state,
-                               BlockReopenQueue *queue, Error **errp)
+							   BlockReopenQueue *queue, Error **errp)
 {
-    return 0;
+	return 0;
 }
 
 static void niovadev_refresh_filename(BlockDriverState *bs)
 {
-    NiovaDevState *s = bs->opaque;
+	NiovaDevState *s = bs->opaque;
 	char uuid_str[UUID_STR_LEN] = {0};
 
 	uuid_unparse(s->xopts.npcx_opts.target_uuid, uuid_str);
 
-    snprintf(bs->exact_filename, sizeof(bs->exact_filename), "niova://%s#%u",
-             uuid_str, s->xopts.npcx_opts.queue_depth);
+	snprintf(bs->exact_filename, sizeof(bs->exact_filename), "niova://%s#%u",
+			 uuid_str, s->xopts.npcx_opts.queue_depth);
 }
 
 static void niovadev_refresh_limits(BlockDriverState *bs, Error **errp)
 {
-    bs->bl.opt_mem_alignment = NIOVADEV_BLOCK_SIZE;
-    bs->bl.request_alignment = NIOVADEV_BLOCK_SIZE;
+	bs->bl.opt_mem_alignment = NIOVADEV_BLOCK_SIZE;
+	bs->bl.request_alignment = NIOVADEV_BLOCK_SIZE;
 	bs->bl.pdiscard_alignment = NIOVADEV_BLOCK_SIZE;
-    bs->bl.max_transfer = NIOVADEV_MAX_XFER_BLKS * NIOVADEV_BLOCK_SIZE;
+	bs->bl.max_transfer = NIOVADEV_MAX_XFER_BLKS * NIOVADEV_BLOCK_SIZE;
 	bs->bl.max_iov = NIOVADEV_MAX_IOV;
-    bs->bl.max_pdiscard = QEMU_ALIGN_DOWN(INT_MAX, NIOVADEV_BLOCK_SIZE);
+	bs->bl.max_pdiscard = QEMU_ALIGN_DOWN(INT_MAX, NIOVADEV_BLOCK_SIZE);
 }
 
 static BlockStatsSpecific *niovadev_get_specific_stats(BlockDriverState *bs)
 {
-    BlockStatsSpecific *stats = g_new(BlockStatsSpecific, 1);
-    NiovaDevState *s = bs->opaque;
+	BlockStatsSpecific *stats = g_new(BlockStatsSpecific, 1);
+	NiovaDevState *s = bs->opaque;
 
 	// defined in qapi
-    stats->driver = BLOCKDEV_DRIVER_TESTDEV;
-    stats->u.testdev = (BlockStatsSpecificTestdev) {
+	stats->driver = BLOCKDEV_DRIVER_TESTDEV;
+	stats->u.testdev = (BlockStatsSpecificTestdev) {
 		.io_size_small = s->stats.io_size_small,
 		.io_size_4096 = s->stats.io_size_4096,
 		.io_size_large = s->stats.io_size_large,
@@ -418,50 +418,50 @@ static BlockStatsSpecific *niovadev_get_specific_stats(BlockDriverState *bs)
 		.iov_cnt_max = s->stats.iov_cnt_max,
 		.qd_cur = s->stats.qd_cur,
 		.qd_max = s->stats.qd_max,
-    };
+	};
 
-    return stats;
+	return stats;
 }
 
 static const char *const niovadev_strong_runtime_opts[] = {
-    NIOVA_OPT_UUID,
-    NIOVA_OPT_VDEV,
-    NIOVA_OPT_QUEUE_DEPTH,
+	NIOVA_OPT_UUID,
+	NIOVA_OPT_VDEV,
+	NIOVA_OPT_QUEUE_DEPTH,
 
-    NULL
+	NULL
 };
 
 static BlockDriver bdrv_testdev = {
-    .format_name              = "niova",
-    .protocol_name            = "niova",
-    .instance_size            = sizeof(NiovaDevState),
+	.format_name			  = "niova",
+	.protocol_name			= "niova",
+	.instance_size			= sizeof(NiovaDevState),
 
-    .bdrv_co_create_opts      = bdrv_co_create_opts_simple,
-    .create_opts              = &bdrv_create_opts_simple,
+	.bdrv_co_create_opts	  = bdrv_co_create_opts_simple,
+	.create_opts			  = &bdrv_create_opts_simple,
 
-    .bdrv_parse_filename      = niovadev_parse_filename,
-    .bdrv_file_open           = niovadev_file_open,
-    .bdrv_close               = niovadev_close,
-    .bdrv_co_getlength        = niovadev_getlength,
-    .bdrv_probe_blocksizes    = niovadev_probe_blocksizes,
-    .bdrv_co_truncate         = niovadev_co_truncate,
-    .bdrv_co_pdiscard         = niovadev_co_pdiscard,
+	.bdrv_parse_filename	  = niovadev_parse_filename,
+	.bdrv_file_open		   = niovadev_file_open,
+	.bdrv_close			   = niovadev_close,
+	.bdrv_co_getlength		= niovadev_getlength,
+	.bdrv_probe_blocksizes	= niovadev_probe_blocksizes,
+	.bdrv_co_truncate		 = niovadev_co_truncate,
+	.bdrv_co_pdiscard		 = niovadev_co_pdiscard,
 
-    .bdrv_co_readv           = niovadev_co_readv,
-    .bdrv_co_writev          = niovadev_co_writev,
+	.bdrv_co_readv		   = niovadev_co_readv,
+	.bdrv_co_writev		  = niovadev_co_writev,
 
-    .bdrv_co_flush_to_disk    = niovadev_co_flush,
-    .bdrv_reopen_prepare      = niovadev_reopen_prepare,
+	.bdrv_co_flush_to_disk	= niovadev_co_flush,
+	.bdrv_reopen_prepare	  = niovadev_reopen_prepare,
 
-    .bdrv_refresh_filename    = niovadev_refresh_filename,
-    .bdrv_refresh_limits      = niovadev_refresh_limits,
-    .strong_runtime_opts      = niovadev_strong_runtime_opts,
-    .bdrv_get_specific_stats  = niovadev_get_specific_stats,
+	.bdrv_refresh_filename	= niovadev_refresh_filename,
+	.bdrv_refresh_limits	  = niovadev_refresh_limits,
+	.strong_runtime_opts	  = niovadev_strong_runtime_opts,
+	.bdrv_get_specific_stats  = niovadev_get_specific_stats,
 };
 
 static void bdrv_testdev_init(void)
 {
-    bdrv_register(&bdrv_testdev);
+	bdrv_register(&bdrv_testdev);
 }
 
 block_init(bdrv_testdev_init);
